cff5f87c2cce4d1f46b9b9f855d78e239e945e64
Andrew Pfundstein <apfundst@spsu.edu>
2014-10-11 16:31:48 -0400

    woking more

---
+++ router.js
@@ -0,0 +1,48 @@
+var http = require('http');
+
+
+var options
+
+http.createServer(function (req, res) {
+ options = {
+  host: req.host,
+  path: req.path
+};
+
+
+
+}).listen(3000, '127.0.0.1')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var req = http.get(options, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+  // Buffer the body entirely for processing as a whole.
+  var bodyChunks = [];
+  res.on('data', function(chunk) {
+    // You can process streamed parts here...
+    bodyChunks.push(chunk);
+  }).on('end', function() {
+    var body = Buffer.concat(bodyChunks);
+    console.log('BODY: ' + body);
+    // ...and/or process the entire body here.
+  })
+});
+
+req.on('error', function(e) {
+  console.log('ERROR: ' + e.message);
+});
