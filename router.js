6b83d539385d6a1380694a2a1021315a3460e47a
Andrew Pfundstein <apfundst@spsu.edu>
2014-10-11 17:33:24 -0400

    CLIENT SEVER AND ROUTER ALL WORK
    TODO: create a client interface

--- router.js
+++ router.js
@@ -1,15 +1,59 @@
-var http = require('http');
 
 
-var options
+//MAP FOR LOOKUO
+var map = {
+	video1: 9000,
+	VidLeft: 1337,
+	VidRight: 9002
+}
 
+
+//NODE REQUIRED MIDDLEWARE
+var http = require('http'),
+	fs = require('fs'),
+    util = require('util');
+
+
+var options;
+
+
+//CREATS A PORT OF THE ROUTE TO RUN ON
 http.createServer(function (req, res) {
+ 
+var vidName = req.url;
+vidName = vidName.slice(1);
+
+var url = "127.0.0.1";
+
  options = {
-  host: req.host,
-  path: req.path
+  host: url,
+  port: map[vidName],
+  path: ""
 };
 
+console.log(options);
+//SEND REQUEST TO THE SERVER WITH THE VIDEO 
+var temp = http.get(options, function(response) {
+  console.log('STATUS: ' + response.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(response.headers));
 
+  // Buffer the body entirely for processing as a whole.
+  // var bodyChunks = [];
+  // response.on('data', function(chunk) {
+  //   // You can process streamed parts here...
+  //   bodyChunks.push(chunk);
+  // }).on('end', function() {
+  //   var body = Buffer.concat(bodyChunks);
+  //   console.log('BODY: ' + body);
+  //   // ...and/or process the entire body here.
+  // })
+	response.pipe(res);
+
+});
+
+temp.on('error', function(e) {
+  console.log('ERROR: ' + e);
+});
 
 }).listen(3000, '127.0.0.1')
 
@@ -27,22 +71,4 @@ http.createServer(function (req, res) {
 
 
 
-var req = http.get(options, function(res) {
-  console.log('STATUS: ' + res.statusCode);
-  console.log('HEADERS: ' + JSON.stringify(res.headers));
 
-  // Buffer the body entirely for processing as a whole.
-  var bodyChunks = [];
-  res.on('data', function(chunk) {
-    // You can process streamed parts here...
-    bodyChunks.push(chunk);
-  }).on('end', function() {
-    var body = Buffer.concat(bodyChunks);
-    console.log('BODY: ' + body);
-    // ...and/or process the entire body here.
-  })
-});
-
-req.on('error', function(e) {
-  console.log('ERROR: ' + e.message);
-});
