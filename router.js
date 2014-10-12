7e348da2ef0505404dbd7fcbeb20ac7249a733f2
Andrew Pfundstein <apfundst@spsu.edu>
2014-10-12 19:14:08 -0400

    asd

--- router.js
+++ router.js
@@ -7,56 +7,79 @@ var map = {
   leftvid: 9003,
   stupid: 9004
 }
-
+var timeDif = function(start, end){
+  return end - start;
+}
 
 //NODE REQUIRED MIDDLEWARE
 var http = require('http'),
-	fs = require('fs'),
+	  fs = require('fs'),
     util = require('util');
 
 
 var options;
+var endTranTime;
+var startTranTime;
+var endlooTime;
+var startlookTime;
 
 
 //CREATS A PORT OF THE ROUTE TO RUN ON
 http.createServer(function (req, res) {
+  startlookTime = new Date();
  
-var vidName = req.url;
-vidName = vidName.slice(1);
+  var vidName = req.url;
+  vidName = vidName.slice(1);
 
-var url = "127.0.0.1";
+  var url = "192.168.1.7";
 
- options = {
-  host: url,
-  port: map[vidName],
-  path: ""
-};
+  options = {
+    host: url,
+    port: map[vidName],
+    path: ""
+  };
+  endlooTime = new Date();
 
-console.log(options);
-//SEND REQUEST TO THE SERVER WITH THE VIDEO 
-var temp = http.get(options, function(response) {
-  console.log('STATUS: ' + response.statusCode);
-  console.log('HEADERS: ' + JSON.stringify(response.headers));
+  console.log("look tim is: " + timeDif(startlookTime, endlooTime));
+  console.log(options);
 
-  // Buffer the body entirely for processing as a whole.
-  // var bodyChunks = [];
-  // response.on('data', function(chunk) {
-  //   // You can process streamed parts here...
-  //   bodyChunks.push(chunk);
-  // }).on('end', function() {
-  //   var body = Buffer.concat(bodyChunks);
-  //   console.log('BODY: ' + body);
-  //   // ...and/or process the entire body here.
-  // })
-	response.pipe(res);
+  //SEND REQUEST TO THE SERVER WITH THE VIDEO 
+  var temp = http.get(options, function(response) {
+    startTranTime = new Date();
+    console.log('STATUS: ' + response.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(response.headers));
 
-});
+    // Buffer the body entirely for processing as a whole.
+    // var bodyChunks = [];
+    // response.on('data', function(chunk) {
+    //   // You can process streamed parts here...
+    //   bodyChunks.push(chunk);
+    // }).on('end', function() {
+    //   var body = Buffer.concat(bodyChunks);
+    //   console.log('BODY: ' + body);
+    //   // ...and/or process the entire body here.
+    // })
+  	response.pipe(res);
 
-temp.on('error', function(e) {
-  console.log('ERROR: ' + e);
-});
+  });
+  res.on('finish', function(){
+     endTranTime = new Date();
+    var dif = timeDif(startTranTime, endTranTime)
+    console.log("the dif is: " + dif);
+  });
 
-}).listen(3000, '127.0.0.1')
+  // temp.on('finish', function(){
+  //   endTranTime = new Date();
+  //   var dif = timeDif(startTranTime, endTranTime)
+  //   console.log("the dif is: " + dif);
+
+  // });
+
+  temp.on('error', function(e) {
+    console.log('ERROR: ' + e);
+  });
+
+}).listen(3000, '192.168.1.7')
 
 
 
